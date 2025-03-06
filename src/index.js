ed60c332c9f04428eab2f69fa9afe6a54ecafab5
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-03-06 11:19:16 +0800

    refactor: convert menu page to DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import home from './pages/home/home.js';
+import menu from './pages/menu/menu.js';
 
 const homeBtn = document.querySelector('.home-btn');
 const menuBtn = document.querySelector('.menu-btn');
@@ -8,6 +9,11 @@ const content = document.querySelector('#content');
 homeBtn.addEventListener('click', () => {
   content.innerHTML = '';
   home();
+});
+
+menuBtn.addEventListener('click', () => {
+  content.innerHTML = '';
+  menu();
 });
 
 home();
---
+++ src/pages/menu/menu.js
@@ -0,0 +1,105 @@
+import './menu.css';
+import wafflesAndChickenImg from '../../assets/waffles-and-chicken.webp'
+import icedSaltedCaramelImg from '../../assets/iced-salted-caramel-affogato.webp';
+import kingFeast from '../../assets/king-feast.webp';
+import whopperJr from '../../assets/whopper-jr.webp';
+
+function createElemWithClass(elemName, className) {
+  const newElem = document.createElement(elemName);
+  newElem.classList.add(className);
+  return newElem;
+}
+
+function menu() {
+  const content = document.querySelector('#content');
+
+  const container = createElemWithClass('div', 'container');
+  content.appendChild(container);
+
+  const menu = {
+    featured: [
+      {
+        imgSrc: wafflesAndChickenImg,
+        title: '2pc. Waffles & Chicken Solo',
+      },
+      {
+        imgSrc: icedSaltedCaramelImg,
+        title: 'Iced Salted Caramel Affogato',
+      },
+      {
+        imgSrc: kingFeast,
+        title: 'King Feast: Mix\'n Feast',
+      },
+      {
+        imgSrc: whopperJr,
+        title: 'Whopper Jr.',
+      },
+    ],
+    bestSellers: [
+      {
+        imgSrc: icedSaltedCaramelImg,
+        title: 'Iced Salted Caramel Affogato',
+      },
+      {
+        imgSrc: wafflesAndChickenImg,
+        title: '2pc. Waffles & Chicken Solo',
+      },
+      {
+        imgSrc: whopperJr,
+        title: 'Whopper Jr.',
+      },
+      {
+        imgSrc: kingFeast,
+        title: 'King Feast: Mix\'n Feast',
+      },
+      {
+        imgSrc: whopperJr,
+        title: 'Whopper Jr.',
+      },
+    ],
+    bundles: [
+      {
+        imgSrc: wafflesAndChickenImg,
+        title: '2pc. Waffles & Chicken Solo',
+      },
+      {
+        imgSrc: icedSaltedCaramelImg,
+        title: 'Iced Salted Caramel Affogato',
+      },
+    ],
+  };
+
+  for (let section in menu) {
+    const container = createElemWithClass('div', 'container');
+    content.appendChild(container);
+
+    const list = createElemWithClass('div', 'list');
+    container.appendChild(list);
+
+    const header = createElemWithClass('h1', 'header');
+    header.textContent = section;
+    list.appendChild(header);
+
+    const cards = createElemWithClass('div', 'cards');
+    list.appendChild(cards);
+
+    for (let item of menu[section]) {
+      const card = createElemWithClass('div', 'card');
+      cards.appendChild(card);
+
+      const image = document.createElement('img');
+      image.src = item.imgSrc;
+      card.appendChild(image);
+
+      const cardTitle = document.createElement('h3');
+      cardTitle.textContent = item.title;
+      card.appendChild(cardTitle);
+
+      const btn = document.createElement('button');
+      btn.textContent = 'Order Now';
+      card.appendChild(btn);
+    }
+  }
+}
+
+export default menu;
